5b59c28486174896b92286fc39587dee20a666a4
dingningq <dingningq@163.com>
2017-04-07 12:45:53 +0800

    a

---
+++ js/index.js
@@ -0,0 +1,30 @@
+
+//->REM
+~function () {
+    var desW = 750,
+        winW = document.documentElement.clientWidth || document.body.clientWidth;
+    if (winW > desW) {
+        document.getElementById('main').style.width = desW + 'px';
+        return;
+    }
+    document.documentElement.style.fontSize = winW / desW * 100 + "px";
+}();
+/*var winH=$(window).height();
+$(".shut").css("height",winH);*/
+$(function(){
+	//些处为展开收缩
+	var ele=$(".info_list>ul>li");
+ele.delegate('a','click',function(e){
+	$(this).next().slideToggle();
+	$(this).parent("li").siblings().find("a").next().hide();
+	
+	//遮罩层
+	$("#tan").click(function(){
+		$(".layer").removeClass("none");
+	})
+	$("#know").click(function(){
+		$(".layer").addClass("none");
+	})
+})
+})
+
